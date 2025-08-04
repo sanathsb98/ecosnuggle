4a7e824480e7d397b02db8f93d1239575ec0234f
sanathsb98 <itssanathsb@gmail.com>
2025-08-04 21:16:19 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,51 @@
 import React from 'react';
 import './Home.css';
 
-import homebanner from '../assets/home-banner.png'; 
-import mobilebanner from '../assets/home-mobile-banner.png';
-import tabbanner from '../assets/home-tab-banner.png';
+import homeBannerDesktop from '../assets/home-banner.png';
+import homeBannerTablet from '../assets/home-tab-banner.png';
+import homeBannerMobile from '../assets/home-mobile-banner.png';
 
 const Home = () => {
   return (
-    <div className='home-container page'>
-      <img src={homebanner} alt='Desktop Banner' className='home-banner desktop-only' />
-      <img src={tabbanner} alt='Tablet Banner' className='home-banner tab-only' />
-      <img src={mobilebanner} alt='Mobile Banner' className='home-banner mobile-only' />
+    <div className="home-page-container">
+      <div className="home-banner-wrapper">
+        <img src={homeBannerDesktop} alt="Home Banner Desktop" className="home-banner desktop" />
+        <img src={homeBannerTablet} alt="Home Banner Tablet" className="home-banner tablet" />
+        <img src={homeBannerMobile} alt="Home Banner Mobile" className="home-banner mobile" />
+
+        <div className="home-overlay">
+          <div className="container">
+            <div className="maxscreenlimit">
+              <div className="home-text">
+                <h1 className="home-banner-title">
+                  Soft beginnings,<br /> made with love.
+                </h1>
+                <p className="home-banner-des">
+                  Sustainable essentials for your<br />
+                  newborn – gentle on the planet,<br />
+                  perfect for your baby. Coming Soon!
+                </p>
+                <button className="notify-button">Notify me</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Home;
+
+
+
+
+
+
+
+
+
+
 
 
 
